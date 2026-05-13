2b20c90983cebac4e9876f4f69cdd35c040502b5
Jackson Vance <jackson.vance2000@gmail.com>
2026-05-13 15:47:33 -0500

    Final Version except old data

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ st.title("Semi-Annual Brian Finishes Last Event")
 
 if st.button(label="Refresh"):
     st.cache_data.clear()
-    st.write(pd.Timestamp.now())
 
 df = get_df()
 st.dataframe(df, hide_index=True)
