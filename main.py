4ae864f8b1b0a53107b6bc1bf0909d5be991efba
Jackson Vance <jackson.vance2000@gmail.com>
2026-05-13 15:46:21 -0500

    testing refresh button again

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import streamlit as st
 from utils import get_df
+import pandas as pd
 st.title("Semi-Annual Brian Finishes Last Event")
 
 
 
 if st.button(label="Refresh"):
     st.cache_data.clear()
-    st.write("Data Cleared")
+    st.write(pd.Timestamp.now())
 
 df = get_df()
 st.dataframe(df, hide_index=True)
